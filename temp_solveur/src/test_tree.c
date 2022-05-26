0904e4820e91b43f1ce0cc35412613a944c05883
LENFANT Quentin <quentin.lenfant@telecomnancy.eu>
2022-05-26 14:44:53 +0200

    Update makefile and test_tree

--- temp_solveur/src/test_tree.c
+++ temp_solveur/src/test_tree.c
@@ -3,152 +3,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-char mots[52][6] = {
-    "DUNES",
-    "DUODI",
-    "DUPEE",
-    "DUPER",
-    "DUPES",
-    "DUPEZ",
-    "DURAT",
-    "DURCI",
-    "DUREE",
-    "DURER",
-    "DURES",
-    "DURIT",
-    "DUSSE",
-    "DUVET",
-    "DYKES",
-    "DYNES",
-    "EBAHI",
-    "EBATS",
-    "EBENE",
-    "EBOUE",
-    "ECALE",
-    "ECART",
-    "ECHEC",
-    "ECHER",
-    "ECHOS",
-    "ECHUE",
-    "ECHUT",
-    "ECLAT",
-    "ECLOS",
-    "ECOLE",
-    "ECOLO",
-    "ECOPA",
-    "ECOPE",
-    "ECRAN",
-    "ECRIA",
-    "ECRIE",
-    "ECRIN",
-    "ECRIS",
-    "ECRIT",
-    "ECROU",
-    "ECRUE",
-    "ECSTA",
-    "ECULE",
-    "ECUMA",
-    "ECUME",
-    "EDENS",
-    "EDITE",
-    "EDITO",
-    "EDITS",
-    "EDUEN",
-    "DEVON",
-    "DEVIN",
-};
-char couleur[52][6] = {
-    "20110",
-    "20001",
-    "20010",
-    "20010",
-    "20010",
-    "20010",
-    "20000",
-    "20001",
-    "20010",
-    "20010",
-    "20010",
-    "20020",
-    "20001",
-    "20210",
-    "20010",
-    "20110",
-    "10001",
-    "10000",
-    "10010",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10002",
-    "10020",
-    "10020",
-    "10022",
-    "10020",
-    "10020",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "10000",
-    "11010",
-    "11100",
-    "11100",
-    "11100",
-    "11002",
-    "22202",
-    "22222",
-};
-
 dico* D;
 abr* A;
 int nb_letters;
 
 int main()
 {
+    // TESTS AVEC DES MOTS DE 4 LETTRES
+    // (pour des longueurs différentes, changer la variable globale "nb_letters" ci-après et choisir un mot dans le dictionnaire correspondant)
+    // -----
+    // Pour "AXEL" et la couleur "2222", doit afficher un seul mot : AXEL                                                               ==> OK
+    // Pour "AAAA" et la couleur "2222", doit afficher aucun mot                                                                        ==> OK
+    // Pour "AXEL" et la couleur "1111", doit afficher aucun mot                                                                        ==> OK
+    // Pour "AAAA" et la couleur "1111", doit afficher aucun mot                                                                        ==> OK
+    // Pour "AXEL" et la couleur "0000", doit afficher tous les mots qui ne contiennent pas de A                                        ==> OK
+    // Pour "AAAA" et la couleur "0000", doit afficher tous les mots qui ne contiennent pas de A                                        ==> OK
+    // Pour "AXEL" et la couleur "1000", doit afficher les mots qui ne commençent pas par A                                             ==> OK
+    // Pour "AAAA" et la couleur "1000", doit afficher aucun mot                                                                        ==> OK
+    // Pour "AXEL" et la couleur "2000", doit afficher seulement les mots commençent par la lettre A                                    ==> OK
+    // Pour "AAAA" et la couleur "2000", doit afficher seulement les mots qui ne contiennent qu'un seul A et qui est au début du mot    ==> OK
 
     nb_letters = 4;
-    D = init_dico();
+    char* wordTest = "AAAA";
+    char* colorTest = "2000";
 
+    D = init_dico();
     A = init_A();
     char buffer[nb_letters + 1];
-    //print_A(A, buffer);
+    //print_A(A, buffer);   // Print pour vérifier le fonctionnement de la fonction d'initialisation de l'arbre
     
     occ_table T;
     init_T(T);
     compteur c;
     init_C(c);
-
-    maj_T(T,"AXEL","2222");
-    MAJ_A(A,T,"AXEL","2222");
+    maj_T(T,wordTest,colorTest);
+    MAJ_A(A,T,wordTest,colorTest);
     print_A(A,buffer);
-    // for (int i = 0; i < 52; i++)
-    // {
-    //     // init_T(T);
-    //     maj_T(T, mots[i], couleur[i]);
-    //     // print_T(T);
-
-    //     printf("%s : \n", mots[i]);
-    //     compteur c;
-    //     init_C(c);
-    //     printf("%d \n", elague(A, T, mots[i], couleur[i], c));
-    //     printf("%d \n", nb_match(A, T, mots[i], couleur[i], c));
-    //     print_A(A, buffer);
-    // }
-
+    
     destroy_A(A);
     destroy_dico(D);
-    return 0;
+    return EXIT_SUCCESS;
 }
