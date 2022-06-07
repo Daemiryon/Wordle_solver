d970d4df456035510863989c2aa09221cb29a11d
Paul Sanchez <paul.sanchez@telecomnancy.eu>
2022-06-07 16:36:49 +0200

    tests dico

--- solveur/src/dictionnary_test.c
+++ solveur/src/dictionnary_test.c
@@ -5,72 +5,154 @@
 #include<string.h>
 
 int nb_letters;
+dico* D;
+abr* A;
 
 int main(){
 
-    nb_letters = 5;
-    
-    dico* dico;
-    dico = init_dico(); 
-    //print_dico(dico);
-    //print_dico_p(dico);
-    
-    /*
-    printf("----SUPPR_DICO----\n");
-    suppr_dico(dico,22);
-    suppr_dico(dico,21);
-    printf("FIRST PRINT\n");
-    print_dico_p(dico);
-    suppr_dico(dico,20);
-    suppr_dico(dico,19);
-    suppr_dico(dico,18);
-    suppr_dico(dico,1);
-    suppr_dico(dico,0);
-    suppr_dico(dico,2);
-    suppr_dico(dico,3);
-    suppr_dico(dico,10);
-    suppr_dico(dico,9);
-    suppr_dico(dico,11);
-    suppr_dico(dico,12);
-    suppr_dico(dico,8);
-    suppr_dico(dico,7);
-    printf("2nd PRINT\n");
-    print_dico_p(dico);
-    suppr_dico(dico,4);
-    suppr_dico(dico,5);
-    suppr_dico(dico,6);
-    suppr_dico(dico,14);
-    suppr_dico(dico,13);
-    printf("3rd PRINT\n");
-    print_dico_p(dico);
-    suppr_dico(dico,15);
-    suppr_dico(dico,16);
-    suppr_dico(dico,17);
-    */
-    
-    /*
-    printf("----POP----\n");
-    printf("Should be S : %s\n",pop(dico,15));
-    printf("Should be A : %s\n",pop(dico,0));
-    printf("Should be Z : %s\n",pop(dico,22));
-    printf("S,A and Z shouldn't be here : \n");
-    print_dico_p(dico);
-    */
+    nb_letters = 4;
+    D = init_dico();
+    A = init_A(); 
+    int keep_alive=0;
 
-   /*
-    printf("----MAJ_DICO----\n");
+    /* VERIFICATION QUE LE DICTIONNAIRE SE SOIT BIEN CHARGÉ : */
+    
+    print_dico(D);
+    print_dico_p(D);
 
+
+    /* --- TEST SUPPR_DICO --- */
+
+
+    suppr_dico(D,1992); //Case - Last removed
+    printf("ACHE (1992) shouldn't be here : \n");
+    print_dico_p(D);
+    suppr_dico(D,0); //Case - First removed
+    printf("SUIS (0) shouldn't be here : \n");
+    print_dico_p(D);
+    suppr_dico(D,1990); //Case - Random removed
+    printf("AGRO (1990) shouldn't be here : \n");
+    print_dico_p(D);
+    for (int i=1;i<D->taille;i++){
+        if(i == 1992 || i==1990){
+            continue;
+        }
+        suppr_dico(D,i);
+    }
+    printf("Only ADJA should remain : \n"); //Case - Only 1 element left
+    print_dico_p(D);
+    
+
+    /* --- TEST POP --- */
+
+
+    // Réinitialisation du dictionnaire 
+    destroy_dico(D);
+    D = init_dico();
+    
+    printf("Should be AGRO : %s\n",pop(D,1990)); //Case - Random removed
+    printf("Should be SUIS : %s\n",pop(D,0)); //Case - First removed
+    printf("Should be ACHE : %s\n",pop(D,1992)); //Case - Last removed
+    printf("AGRO (1990), SUIS (0) and ACHE (1992) shouldn't be here : \n");
+    //print_dico_p(D);
+    for (int i=1;i<D->taille;i++){
+        if(i == 1992 || i==1990){
+            continue;
+        }
+        suppr_dico(D,i);
+    }
+    printf("Should be ADJA : %s \n",pop(D,D->taille-2)); //Case - Only 1 element left
+
+
+    /* --- TEST MAJ_DICO --- */
+
+
+    // Réinitialisation du dictionnaire 
+    destroy_dico(D);
+    D = init_dico();
     occ_table T;
     init_T(T);
-    maj_T(T,"AVION","00000");
     
-    maj_dico(dico,T);
+    // Case - colour matching = 0
 
-    print_dico_p(dico);
-    print_T(T);
-    */
+    maj_T(T,"ACHE","0000");
+    MAJ_A(A,T,"ACHE","0000");
+    maj_dico(D);
+    printf("Any words with A,C,H and E shouldn't be here :\n");
+    print_dico_p(D);
+    
+    // Case - colour matching = 1
 
-    destroy_dico(dico);
+    // Réinitialisation du dictionnaire et T
+    destroy_dico(D);
+    destroy_A(A);
+    D = init_dico();
+    init_T(T);
+    A = init_A();
 
+    maj_T(T,"ACHE","1111");
+    MAJ_A(A,T,"ACHE","1111");
+    maj_dico(D);
+    printf("Only ACHE should be here :\n");
+    print_dico_p(D);
+    
+    // Case - colour matching = 2
+
+    // Réinitialisation du dictionnaire et T 
+    destroy_dico(D);
+    destroy_A(A);
+    D = init_dico();
+    init_T(T);
+    A = init_A();
+
+    maj_T(T,"ACHE","2222");
+    MAJ_A(A,T,"ACHE","2222");
+    maj_dico(D);
+    printf("Only ACHE should be here :\n");
+    print_dico_p(D);
+
+
+    /* --- TEST MAJ_DICO2 --- */
+
+
+    // Réinitialisation du dictionnaire et T
+    destroy_dico(D);
+    D = init_dico();
+    init_T(T);
+    
+    // Case - colour matching = 0
+
+    maj_T(T,"ACHE","0000");
+    maj_dico2(T,D);
+    printf("Any words with A,C,H and E shouldn't be here :\n");
+    print_dico_p(D);
+
+    // Case - colour matching = 1
+
+    // Réinitialisation du dictionnaire et T
+    destroy_dico(D);
+    D = init_dico();
+    init_T(T);
+
+    maj_T(T,"ACHE","1111"); 
+    maj_dico2(T,D);
+    printf("Only ACHE should be here :\n");
+    print_dico_p(D);
+
+    // Case - colour matching = 2
+
+    // Réinitialisation du dictionnaire et T 
+    destroy_dico(D);
+    D = init_dico();
+    init_T(T);
+
+    maj_T(T,"ACHE","2222"); 
+    maj_dico2(T,D);
+    printf("Only ACHE should be here :\n");
+    print_dico_p(D);
+    
+
+    destroy_A(A);
+    destroy_dico(D);
     return EXIT_SUCCESS;
 }
